3c8e503922a8b31fd83c51d88180020c698cc297
yujiahan <yujiahan_biancheng@163.com>
2023-12-30 18:52:15 +0800

    blogcommit

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 npm run build
 
 # 进入生成的文件夹
-cd .vuepress/dist
+#cd .vuepress/dist
 
 git remote add  origin https://github.com/yujiahan2018/myblogsite.git
 git init
@@ -10,5 +10,5 @@ git add -A
 git commit -m 'blogcommit'
 
 # 发布到 https://<USERNAME>.github.io/<REPO>
-git push origin main https://github.com/yujiahan2018/myblogsite.git
+git push origin main
 
